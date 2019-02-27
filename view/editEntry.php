a6166ca9ada118a6a77623bad96732788fd4eb73
Sourigna-Vannapho <sourignavannapho@gmail.com>
2019-02-27 23:20:39 +0100

    Initialization

---
+++ view/editEntry.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <title>Billet simple pour l'Alaska</title>
+        <meta charset="utf-8" />
+        <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=6e98t8myxa3tv4dorwagpgmtq22yc8nu3hu5pk5iubtw4vkb"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
