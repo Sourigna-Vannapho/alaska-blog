cf1f2bdf9e996f7b94b7a2cb466b9a2a6d458174
Sourigna <sourignavannapho@gmail.com>
2019-03-03 11:56:40 +0100

    Post formatting + Return to showPosts from single post

--- view/showPostView.php
+++ view/showPostView.php
@@ -5,13 +5,14 @@
 
 	<div>
 		<h2>
-			<?= htmlspecialchars($post['title']) . $post['date_creation'] ?> 
+			<?= htmlspecialchars($post['title'])?> 
 		</h2>
+		<p><?= 'Posté le ' . $post['date_creation']?> </p>
 		<p>
 			<?= nl2br(htmlspecialchars($post['content'])) ?>
 		</p>
 	</div>
-
+<a href='index.php?action=showPosts'>Retour à l'accueil</a>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
--- view/showPostsView.php
+++ view/showPostsView.php
@@ -9,8 +9,9 @@ while ($data = $posts->fetch())
 ?>
 	<div>
 		<h2>
-			<a href='index.php?action=post&amp;id=<?= $data['id']?>'><?= htmlspecialchars($data['title']) . $data['date_creation'] ?></a>
+			<a href='index.php?action=post&amp;id=<?= $data['id']?>'><?= htmlspecialchars($data['title']) ?></a>
 		</h2>
+		<p><?= 'Posté le ' . $data['date_creation']?> </p>
 		<p>
 			<?= nl2br(htmlspecialchars($data['content'])) ?>
 		</p>
