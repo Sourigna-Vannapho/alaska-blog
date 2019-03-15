3f87b2eef0c83149172ce1c37ce539b0b6bf5307
Sourigna <sourignavannapho@gmail.com>
2019-03-15 23:11:12 +0100

    Styling for comment report

--- view/admin_comment_report.php
+++ view/admin_comment_report.php
@@ -6,11 +6,15 @@
 while ($data = $req->fetch())
 	{ 
 	if ($data['report_status']>0){?>
-		<p><?= $data['content']?>
+		<p class=reportedComment><?= $data['content']?>
 		</p>
-		<p>Nombre de signalement(s) : <?= $data['report_status'] ?></p>
-		<p>Posté par <?= $data['pseudo'] ?> dans l'entrée <?= htmlspecialchars($data['title']) ?></p>	
-		<a href='index.php?action=post&amp;id=<?= $data['billet_id'].'#'. $data['comment_id']?>'>Voir le commentaire</a>
+		<div id='infoReportedComment'>
+			<div id='reportLeft'>
+				<p class='info'>Nombre de signalement(s) : <?= $data['report_status'] ?></p>
+				<p class='info'>Posté par <?= $data['pseudo'] ?> dans l'entrée <?= htmlspecialchars($data['title']) ?></p>	
+			</div>
+			<a class='info' href='index.php?action=post&amp;id=<?= $data['billet_id'].'#'. $data['comment_id']?>'>Voir le commentaire</a>
+		</div>
 
 	<?php
 	}}
