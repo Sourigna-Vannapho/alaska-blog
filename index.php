7340905648a9c5f8a3738be4408b853da7b0e9ef
Sourigna <sourignavannapho@gmail.com>
2019-03-20 19:20:55 +0100

    Readjusted faulty routing

--- index.php
+++ index.php
@@ -6,67 +6,72 @@ if(!isset($_SESSION))
 
 require('controller/controller.php');
 try{
-if (isset($_GET['action'])) {
-	if ($_GET['action'] == 'showPosts'){
-		showPosts();
-	}
-	else if ($_GET['action'] == 'post' && $_GET['id'] > 0){
-		singlePost();
-	}
-	else if ($_GET['action'] == 'register'){
-		register();
-	}
-	else if ($_GET['action'] == 'register_confirm'){
-		registerConfirm();
-	}
-	else if ($_GET['action'] == 'login_confirm'){
-		loginConfirm();
-	}
-	else if ($_GET['action'] == 'logout'){
-		logoutConfirm();
-	}
-	else if ($_SESSION){
-		if ($_SESSION['authority'] >= 1){
-			if ($_GET['action'] == 'report_comment'){
-		commentReportConfirm();
-			}
+	if (isset($_GET['action'])) {
+		if ($_GET['action'] == 'showPosts'){
+			showPosts();
 		}
-		if ($_GET['action'] == 'comment_confirm' && $_GET['id'] > 0){
-		commentConfirm();
+		else if ($_GET['action'] == 'post' && $_GET['id'] > 0){
+			singlePost();
 		}
-		else if ($_SESSION['authority'] == 2){
-			if ($_GET['action'] == 'add_entry'){
-			addEntry();
-			}
-			else if ($_GET['action'] == 'add_entry_confirm'){
-				if (isset($_GET['id'])){
-					updateEntryConfirm();
+		else if ($_GET['action'] == 'register'){
+			register();
+		}
+		else if ($_GET['action'] == 'register_confirm'){
+			registerConfirm();
+		}
+		else if ($_GET['action'] == 'login_confirm'){
+			loginConfirm();
+		}
+		else if ($_GET['action'] == 'logout'){
+			logoutConfirm();
+		}
+		else if ($_SESSION){
+			if ($_SESSION['authority'] >= 1){
+				if ($_GET['action'] == 'report_comment'){
+				commentReportConfirm();
 				}
-				else{
-					addEntryConfirm();
+			
+				else if ($_GET['action'] == 'comment_confirm' && $_GET['id'] > 0){
+				commentConfirm();
 				}
-			}
-			else if ($_GET['action'] == 'delete_entry'){
-				deleteEntryConfirm();
-			}
-			else if ($_GET['action'] == 'admin_comment'){
-				adminComment();
-			}
-			else if ($_GET['action'] == 'delete_comment'){
-				deleteCommentConfirm();
-			}
+					if ($_SESSION['authority'] == 2){
+						if ($_GET['action'] == 'admin_panel'){
+							adminPanel();
+						}
+						else if ($_GET['action'] == 'add_entry'){
+						addEntry();
+						}
+						else if ($_GET['action'] == 'add_entry_confirm'){
+							if (isset($_GET['id'])){
+								updateEntryConfirm();
+							}
+							else{
+								addEntryConfirm();
+							}
+						}
+						else if ($_GET['action'] == 'delete_entry'){
+							deleteEntryConfirm();
+						}
+						else if ($_GET['action'] == 'admin_comment'){
+							adminComment();
+						}
+						else if ($_GET['action'] == 'delete_comment'){
+							deleteCommentConfirm();
+						}
+						else{
+							showPosts();
+						}
+					}
 			else{
 				showPosts();
+				}
 			}
 		}
-	}
-	else {
+	}	
+	else{
 		showPosts();
 	}
 }
-else{
-	showPosts();
-}}
 catch(Exception $e){
 	$errorMsg = $e->getMessage();
 	require('view/errorView.php');
